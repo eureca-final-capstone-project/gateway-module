9a069276d0d2995a77dbda3ba97b737f7a80d838
Jang-GO <jang8195@naver.com>
2025-07-11 16:59:55 +0900

    feat: cors 설정 추가

---
+++ src/main/java/eureca/capstone/project/gateway/config/WebConfig.java
@@ -0,0 +1,32 @@
+package eureca.capstone.project.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:5500",
+                "http://127.0.0.1:5500",
+                "https://visiblego.com",
+                "https://www.visiblego.com",
+                "https://d11zul423xp1v6.cloudfront.net"
+        ));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
