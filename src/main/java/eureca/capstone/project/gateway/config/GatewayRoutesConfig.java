770f018a57f2cc5bed4a28423a2672014613aebd
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-17 13:02:12 +0900

    feat : 클라이언트, admin 모듈 분리, CORS 개발서버 및 배포 서버 반영

--- src/main/java/eureca/capstone/project/gateway/config/GatewayRoutesConfig.java
+++ src/main/java/eureca/capstone/project/gateway/config/GatewayRoutesConfig.java
@@ -13,29 +13,9 @@ public class GatewayRoutesConfig {
         return builder.routes()
                 .route("orchestrator-service", r -> r.path("/orchestrator/**")
                         .uri("http://capstone-real-alb-1484290202.ap-northeast-2.elb.amazonaws.com"))
-                .route("user-service", r -> r.path("/user/**")
-                        .uri("http://13.124.4.121:8080"))
-                .route("alarm-service", r -> r.path("/alarm/**")
-                        .uri("http://15.164.82.42:8080"))
-                .route("auth-service", r -> r.path("/auth/**")
-                        .uri("http://43.201.165.118:8080"))
-                .route("transaction-feed-service", r -> r.path("/transaction-feed/**")
-                        .uri("http://3.36.18.51:8080"))
-                .route("pay-service", r -> r.path("/pay/**")
-                        .uri("http://52.78.14.23:8080"))
-                .route("statistics-service", r -> r.path("/statistics/**")
-                        .uri("http://43.200.49.85:8080"))
                 .route("admin-service", r -> r.path("/admin/**")
                         .uri("http://3.39.218.134:8080"))
                 .build();
-
-
-
-
-
-
-
-
 
 
     }
--- src/main/java/eureca/capstone/project/gateway/config/WebConfig.java
+++ src/main/java/eureca/capstone/project/gateway/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 import java.util.Arrays;
 
 @Configuration
@@ -15,11 +16,8 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOriginPatterns(Arrays.asList(
-                "http://localhost:5500",
-                "http://127.0.0.1:5500",
-                "https://visiblego.com",
-                "https://www.visiblego.com",
-                "https://d11zul423xp1v6.cloudfront.net"
+                "https://d11zul423xp1v6.cloudfront.net",
+                "https://ureca-final.com/"
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
