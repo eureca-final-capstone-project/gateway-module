80c750ef78b0ee88c2c51b98801e2927a4b84019
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-18 09:43:23 +0900

    feat : 카카오 콜백 경로, 헤더 추가

--- src/main/java/eureca/capstone/project/gateway/config/GatewayRoutesConfig.java
+++ src/main/java/eureca/capstone/project/gateway/config/GatewayRoutesConfig.java
@@ -11,11 +11,20 @@ public class GatewayRoutesConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                // 카카오 로그인 시작 요청
                 .route("orchestrator-kakao-login", r -> r.path("/orchestrator/oauth2/authorization/kakao")
                         .filters(f -> f.stripPrefix(1)) // "/orchestrator" 제거
                         .uri("http://capstone-real-alb-1484290202.ap-northeast-2.elb.amazonaws.com"))
+                // 카카오 로그인 콜백 요청
+                .route("orchestrator-kakao-callback", r -> r.path("/orchestrator/login/oauth2/code/kakao")
+                        .filters(f -> f.stripPrefix(1) // "/orchestrator" 제거
+                                .addRequestHeader("X-Forwarded-Prefix", "/orchestrator") // baseUrl 보정용 헤더 추가
+                        ) // "/orchestrator" 제거
+                        .uri("http://capstone-real-alb-1484290202.ap-northeast-2.elb.amazonaws.com"))
+
                 .route("orchestrator-service", r -> r.path("/orchestrator/**")
                         .uri("http://capstone-real-alb-1484290202.ap-northeast-2.elb.amazonaws.com"))
+
                 .route("admin-service", r -> r.path("/admin/**")
                         .uri("http://3.39.218.134:8080"))
                 .build();
